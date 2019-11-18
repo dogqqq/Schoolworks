2aabbf87e891fca2ec8db3c60154dae5adc4bc35
CrazyFire <fdff87554@gmail.com>
2019-11-18 13:49:16 +0800

    add b.cpp code for test 11/16

--- icpcPrepare/b.cpp
+++ icpcPrepare/b.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int main(){
-    
+    long long int N, S, T;
+    int check;
+    while( cin >> N >> S >> T ){
+        check = 0;
+        for( int i = 0; i < N; i++ ){
+            if( T == S * pow(2, i) ){
+                cout << i << endl;
+                check = 1;
+                break;
+            }
+        }
+        if( check == 0 ){
+            cout << "-1" << endl;
+        }
+    }
 
     return 0;
 }
